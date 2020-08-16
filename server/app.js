d24bbc0363a70ef2d5850b4b72f161496e449edb
jjfitzpatrick <john.fitzpatrick.v@gmail.com>
2020-08-16 11:20:25 -0400

    Template out server dir structure

--- server/app.js
+++ server/app.js
@@ -1,9 +1,20 @@
-const express = require("express");
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
-const port = process.env.PORT || 3000;
-const ip = process.env.IP || '127.0.0.1';
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.get("/", (req, res) => res.send("Welcome to fitzpatrick.io"));
+var app = express();
 
-app.listen(port, ip, () => console.log(`App listening on ${ip}:${port}!`));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
