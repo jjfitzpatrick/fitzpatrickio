2baa383901a62e1111db2f665c613780175a50d0
jjfitzpatrick <john.fitzpatrick.v@gmail.com>
2020-08-17 02:06:21 -0400

    Add favicon assets and support in HTML markup

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,15 +1,23 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
+import Head from 'next/head'
 
 import theme from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider value="light">
-        <CSSReset />
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+
+      </Head>
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider value="light">
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </>
   )
 }
 
--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -9,7 +9,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+          <link rel="manifest" href="/static/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#2b5797" />
+          <meta name="msapplication-config" content="/static/favicons/browserconfig.xml" />
+          <meta name="theme-color" content="#ffffff" />
+
+          {/* TODO: Complete yandex verification */}
+          <meta content="" name="yandex-verification" />
+
+          {/* TODO: Complete google site verification */}
+          <meta content="" name="google-site-verification" />
+        </Head>
         <body>
           <Main />
           <NextScript />
