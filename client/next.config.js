42d435cbd986364f7cb2d83794e6dd9c53841d54
jjfitzpatrick <john.fitzpatrick.v@gmail.com>
2020-08-24 23:45:09 -0400

    Update next.js config

--- client/next.config.js
+++ client/next.config.js
@@ -1,13 +1,29 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants')
 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
+    /* development only config options here */
     return {
-      /* development only config options here */
     }
   }
 
+  else if (phase === PHASE_PRODUCTION_SERVER) {
+    /* config options for production server here */
+    return {
+      async redirects() {
+        return [
+          {
+            source: '/',
+            destination: '/about',
+            permanent: false,
+          },
+        ]
+      },
+    }
+  }
+
+  /* config options for all phases except development here */
   return {
-    /* config options for all phases except development here */
+    
   }
 }
