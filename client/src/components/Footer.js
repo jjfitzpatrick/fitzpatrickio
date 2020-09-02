2c07e6f0385f76ebc7092ae78132ef31c608ecb4
jjfitzpatrick <john.fitzpatrick.v@gmail.com>
2020-09-02 13:08:50 -0400

    Convert Footer component from chakra-ui to grommet

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,58 +1,39 @@
-import {
-  Box,
-  Divider,
-  Link,
-  SimpleGrid,
-  Flex,
-} from '@chakra-ui/core'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Box, Anchor } from 'grommet'
+import { Share, Home } from 'grommet-icons';
   
 const Footer = () => {
   return (
     <>
-    <Flex justifyContent="center">
-      <Divider
-        width="97%"
-        alignItems="center" />
-    </Flex>
-    
-    <Flex
-      justifyContent="center"
-      m={4}
-    >
-      <SimpleGrid columns={3} spacing={10}>
-        <Box>
-          <Link 
-            px={5}
-            href="https://github.com/jjfitzpatrick"
-            title="GitHub"
-            isExternal>
-            GitHub <ExternalLinkIcon />
-          </Link>
-        </Box>
-        <Box>
-          <Link
-            px={5}
-            href="https://www.linkedin.com/in/mr-john-fitzpatrick"
-            title="LinkedIn"
-            isExternal>
-            LinkedIn <ExternalLinkIcon />
-          </Link>
-        </Box>
-        <Box>
-          <Link href="https://fitzpatrick.io"
-            title="fitzpatrick.io">
-            fitzpatrick.io
-          </Link>
-        </Box>
-      </SimpleGrid>
-    </Flex>
-    
-    <Flex justifyContent="center" paddingBottom={2}>
-      <Divider
-        width="97%"
-        alignItems="center" />
-    </Flex>
+      {/* Horizontal divider, width 97% */}
+      <Box
+        align="center"
+        as="footer"
+        direction="row"
+        flex="false"
+        gap="medium"
+        justify="center"
+        pad="medium"
+      >
+        <Anchor
+          href="https://github.com/jjfitzpatrick"
+          label="GitHub"
+          target="_blank"
+          icon={<Share />}>
+        </Anchor>
+        <Anchor
+          href="https://www.linkedin.com/in/mr-john-fitzpatrick"
+          label="LinkedIn"
+          target="_blank"
+          icon={<Share />}>
+        </Anchor>
+        <Anchor
+          href="https://fitzpatrick.io"
+          label="fitzpatrick.io"
+          target="_self"
+          icon={<Home />}>
+        </Anchor>
+      </Box>
+      {/* Horizontal divider, width 97% */}
     </>
   )
 }
