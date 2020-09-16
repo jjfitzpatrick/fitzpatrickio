c8451b4687f8d8d21be4da147df4f8c07074aab8
jjfitzpatrick <john.fitzpatrick.v@gmail.com>
2020-09-15 21:40:43 -0400

    Update github client to properly pull issues and add fetch commits

--- client/src/data/githubClient.js
+++ client/src/data/githubClient.js
@@ -4,7 +4,7 @@ const owner = 'jjfitzpatrick';
 const repo = 'fitzpatrickio';
 
 const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN
+  auth: process.env.GITHUB_TOKEN,
 });
 
 const GetIssues = () => {
@@ -12,13 +12,13 @@ const GetIssues = () => {
 };
 
 const GetMilestones = () => {
-  return octokit.issues.listMilestones({ owner, repo });
+  const state = 'all';
+
+  return octokit.issues.listMilestones({ owner, repo, state });
 };
 
-export { GetIssues, GetMilestones };
+const CompareCommits = (base, head) => {
+  return octokit.repos.compareCommits({ owner, repo, base, head });
+};
 
-// https://api.github.com
-
-// Accept: application/vnd.github.v3+json
-
-// All timestamps return in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ
+export { CompareCommits, GetIssues, GetMilestones };
