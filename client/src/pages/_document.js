31798f43905b8ba275c451eb922aef8df9eb412a
jjfitzpatrick <john.fitzpatrick.v@gmail.com>
2020-11-10 16:06:28 -0500

    Add gtag script to _document head

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+import { GA_TRACKING_ID } from '../lib/gtag';
 
 class MyDocument extends Document {
   static async getInitialProps (ctx) {
@@ -36,6 +37,23 @@ class MyDocument extends Document {
       <>
         <Html>
           <Head>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+              }}
+            />
             <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
