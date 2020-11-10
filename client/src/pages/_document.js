5db07cefe0c20a3f83a0c6134149007741ce5b0f
John Fitzpatrick <john.fitzpatrick.v@gmail.com>
2020-11-10 16:17:47 -0500

    Dewdrop (#83)
    * Resolve FOUC (flicker of unstyled content) on first load due to SSR and styled-components
    
    * Add MIT License
    
    * Update top level README
    
    * Move README content up one level
    
    * Move LICENSE file up one level
    
    * Delete nodejs app
    
    * Create .NET Core web API and add swagger
    
    * Update gitignore
    
    * Add gtag lib
    
    * Add ENV variable for google analytics
    
    * Add gtag script to _document head

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+import { GA_TRACKING_ID } from '../lib/gtag';
 
 class MyDocument extends Document {
   static async getInitialProps (ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const sheet = new ServerStyleSheet();
+
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          sheet.collectStyles(<App {...props} />),
+      });
+    
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return { ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render () {
@@ -12,6 +37,23 @@ class MyDocument extends Document {
       <>
         <Html>
           <Head>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+              }}
+            />
             <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
